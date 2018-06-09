4a0bb6f59ef58485fd34566aa957d2225799795d
hibbahagh <hibbahagh@gmail.com>
2018-06-09 05:18:16 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,24 @@
 # Code your instances here
+class GardenGnome
+  
+  def initialize(name, age, gluten_allergy, personality = "evil") 
+   @name = name
+   @age = age
+   @gluten_allergy = gluten_allergy
+   @personality = personality
+   @hat_color = hat_color
+  end
+  
+  attr_reader :name
+  attr_writer :name
+  
+  attr_reader :age
+  attr_writer :age
+  
+  attr_reader :gluten_allergy
+  attr_writer :gluten_allergy
+  
+  attr_reader :personality
+  
+  
+  
